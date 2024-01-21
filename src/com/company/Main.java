774aa6dacc5e420a05f87f475390270908216c24
Zeogonal <minzia@mail.ru>
2024-01-21 18:33:37 +0300

    IF&&

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,32 @@ package com.company;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
-        List<Integer> newList = new ArrayList<>();
-        for (int x : list) {
-            if (x % 2 != 0 || x <= 0) continue;
-            newList.add(x);
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> intListFiltered1 = new ArrayList<>();
+        List<Integer> intListFiltered2 = new ArrayList<>();
+
+        for (int x :
+                intList) {
+            if (x > 0) {
+                intListFiltered1.add(x);
+            }
         }
-        Collections.sort(newList);
-        for (int i : newList) System.out.println(i);
+
+        for (int x :
+                intListFiltered1) {
+            if (x % 2 == 0) {
+                intListFiltered2.add(x);
+            }
+        }
+
+        intListFiltered2.sort((a, b) -> a.compareTo(b));
+
+        for (int i:
+                intListFiltered2) {
+            System.out.println(i);
+        }
     }
 }
