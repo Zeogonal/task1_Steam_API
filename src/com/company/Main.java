df128a006f86922eaad74c2994636f41a720ae6a
Zeogonal <105427889+Zeogonal@users.noreply.github.com>
2024-01-22 17:44:17 +0300

    &&

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,34 +1,22 @@
 package com.company;
 
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> intListFiltered = new ArrayList<>();
 
-        List<Integer> intListFiltered1 = new ArrayList<>();
-        List<Integer> intListFiltered2 = new ArrayList<>();
-
-        for (int x :
-                intList) {
-            if (x > 0) {
-                intListFiltered1.add(x);
+        for (int x :intList) {
+            if (x > 0 && x % 2 == 0) {
+                intListFiltered.add(x);
             }
         }
+        intListFiltered.sort(Integer::compareTo);
 
-        for (int x :
-                intListFiltered1) {
-            if (x % 2 == 0) {
-                intListFiltered2.add(x);
-            }
-        }
-
-        intListFiltered2.sort((a, b) -> a.compareTo(b));
-
-        for (int i:
-                intListFiltered2) {
-            System.out.println(i);
+        for (int x:
+                intListFiltered) {
+            System.out.println(x);
         }
     }
 }
